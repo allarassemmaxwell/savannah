5487dfde87406340f28ac1a3a4adbb29ed2231b4
Maxime <allarassemmaxwell@gmail.com>
2024-09-27 01:37:56 +0300

    Updating the deployment and add also the index.html file.......

--- backend/settings.py
+++ backend/settings.py
@@ -30,11 +30,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # Debug mode
 # DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['savanah.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['savanah.us-east-1.elasticbeanstalk.com', 'localhost']
 
 
 # Application definition
